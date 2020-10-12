27efbcf54fd93dc7d1a5e1ccc27236e3d4deb421
Matias <matiasezeretondo@gmail.com>
2020-10-12 16:38:42 -0300

    FIXED - GetCoins y Top 5

---
+++ server/controllers/InstrumentController.js
@@ -0,0 +1,104 @@
+import fetch from 'node-fetch'
+
+const token = '1ff62accbf7df9289c6f059f0e86d3479bf9138286a87a4d1b97c9ce6bd222a1'; //Token fijo
+const totalCoins = 30;
+
+var globalCoins = []; // Variable para pasar coins entre endpoints.
+var globalPrices = []; // Variable para pasar prices entre endpoints.
+
+// Devuelve TOP 30 coins.
+export const getCoins = async (req, res) => {
+    try
+    {        
+        let apiURL = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=30&tsym=USD";
+        let returnArray = [];
+
+        const response = await fetch(apiURL, {
+            method: 'GET',            
+        });
+
+        const coins = await response.json();
+
+        for(let i = 0; i < totalCoins; i++){  
+            returnArray.push(coins['Data'][`${i}`]['RAW']);
+        }
+
+        globalCoins = returnArray;
+        res.send(returnArray);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error en el getCoin'
+        });
+    }
+}
+
+// A todas las coins en memoria, les agrega Full Data.
+export const getPrices = async (req, res) => {
+    try
+    {
+        let coins = globalCoins;        
+        let symbols = [];
+        let prices = [];
+        
+        coins.forEach(elements => {
+            symbols.push(elements['USD']['FROMSYMBOL']);
+        });
+        
+        let apiURL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms='${symbols}'&tsyms=USD`;
+
+            let response = await fetch(apiURL, {
+                method: 'GET',
+                headers: {
+                    'authorization' : token
+                }
+            });
+            prices.push(await response.json());   
+        
+        globalPrices = prices;
+        res.send(prices);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error en el getPrices'
+        });
+    }    
+}
+
+// Devuelve el TOP y BOTTOM 5
+export const getTopAndBottom = async (req, res) => {
+    try
+    {
+        if (globalCoins != []) {                           
+            let booleanParam = req.query.booleanParam;
+            let TABArray = [];
+
+            globalCoins.sort((a, b) => (a['USD']['CHANGEPCTDAY'] > b['USD']['CHANGEPCTDAY']) ? 1 : ((b['USD']['CHANGEPCTDAY'] > a['USD']['CHANGEPCTDAY']) ? -1 : 0));
+            
+            console.log(booleanParam);
+
+            if(booleanParam == 'False'){
+                for(let i = 0; i < 5; i++){
+                    TABArray.push(globalCoins[i]);
+                }
+            } 
+            else 
+            {
+                if(booleanParam == "True"){
+                    for(let j = globalCoins.length - 1; j > (globalCoins.length - 6); j--){
+                        TABArray.push(globalCoins[j]);
+                    }
+                }   
+            }
+            res.send(TABArray);        
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error en el getPrices'
+        });
+    }    
+}
--- server/routes/instruments.routes.js
+++ server/routes/instruments.routes.js
@@ -1,11 +1,16 @@
-import { getInstruments, getMarketHistory, getWinnersAndLosers } from '../controllers/Instruments.controller';
+//import { getInstruments, getMarketHistory, getWinnersAndLosers, getWinnersAndLosers } from '../controllers/Instruments.controller';
+import { getCoins, getPrices, getTopAndBottom } from '../controllers/InstrumentController';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/all', getInstruments);
-router.get('/marketHistory', getMarketHistory);
-router.get('/winnersAndLosers', getWinnersAndLosers);
+//router.get('/all', getInstruments);
+//router.get('/marketHistory', getMarketHistory);
+//router.get('/winnersAndLosers', getWinnersAndLosers);
+
+router.get('/getTopAndBottom', getTopAndBottom);
+router.get('/getPrices', getPrices);
+router.get('/getCoins', getCoins);
 
 export default router;
 
