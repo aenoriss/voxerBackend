98091e2b39aa363d55effaa46c78e3852e0092a2
SkyWatcher22 <ramirocanadas@gmail.com>
2020-10-20 14:20:40 -0300

    cambiado a puerto 4000

--- server/server.js
+++ server/server.js
@@ -14,12 +14,10 @@ app
 
 app.use("/api", instrumentsRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-
 
 export default app;
